297ae809e58d53443a895bf5246e4b3a1e4e02fd
lx4556332 <lx4556332@126.com>
2020-03-22 22:57:21 +0800

    登陆表单布局

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <!--头像区-->
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt /> 
+        <img src="../assets/logo.png" alt />
       </div>
+      <!--登陆表单区域-->
+      <el-form label-width="0" class="login_form">
+        <!--用户名-->
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登陆</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -31,9 +48,9 @@ export default {};
     border-radius: 50%;
     padding: 10px;
     box-shadow: 0 0 10px #ddd;
-    position:absolute;
+    position: absolute;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     background-color: #fff;
     img {
       width: 100%;
@@ -42,5 +59,19 @@ export default {};
       background-color: #eee;
     }
   }
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form
+{
+  position:absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing:border-box;
+
+
 }
 </style>
