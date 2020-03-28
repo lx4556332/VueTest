54e532223ca98a652ea8abb87640903f4fed03ad
lx4556332 <lx4556332@126.com>
2020-03-28 12:04:52 +0800

    路由导航卫士，强制跳转

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,21 @@ import Home from '../components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router= new Router({
   routes: [
    {path:'/',redirect:'/login'},
    {path:'/login',component:Login},
    {path:'/home',component:Home}
   ]
 })
+
+router.beforeEach((to,from,next)=>{
+   //to 要访问的路径
+   //from 从哪个路径跳转而来
+   //next 是一个函数，表示放行 next('/login') 强制跳转
+   if(to.path==='/login')  return next()
+   const tokenStr=window.sessionStorage.getItem('token')
+   if(!tokenStr) return next('/login')
+   next()
+})
+export default router
